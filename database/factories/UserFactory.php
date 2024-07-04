1bafff4345228226bc92516eae6fb28684d0cb9d
Dmitry552 <foker552@gmail.com>
2024-07-04 10:41:05 +0300

    Completed migrations and seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,12 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
@@ -26,19 +21,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'phone' => '+380' . fake()->unique()->numberBetween(11111111, 99999999),
+            'position_id' => fake()->numberBetween(1, 4),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
---
+++ database/migrations/2024_07_04_070008_update_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('password');
+            $table->dropColumn('email_verified_at');
+            $table->dropColumn('remember_token');
+
+            $table->unsignedBigInteger('position_id')->nullable();
+            $table->string('phone')->unique();
+            $table->string('photo')->nullable();
+
+            $table->foreign('position_id')
+                ->references('id')
+                ->on('positions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('position_id');
+            $table->dropColumn('phone');
+            $table->dropColumn('photo');
+
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+        });
+    }
+};
